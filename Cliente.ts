d78174bb67f8073783b50551490b761930db1412
Angelo Luz <angelogl@gmail.com>
2025-08-21 11:32:17 -0300

    Adiciona alterações recentes do projeto

--- Cliente.ts
+++ Cliente.ts
@@ -1,4 +1,13 @@
 export class Cliente{
-    nome: string = "";
-    telefone: string = "";
+    nome: string;
+    telefone: string;
+
+    /**
+     * Método especial que executa apenas
+     * uma vez, que é ao criar o objeto
+     */
+    constructor(nomeCliente: string){
+        this.nome = nomeCliente;
+        this.telefone = "";
+    }
 }
--- Main.ts
+++ Main.ts
@@ -1,3 +1,13 @@
 import { Cliente } from "./Cliente";
 
-const cli: Cliente = new Cliente()
+const cli: Cliente = new Cliente("Gladimir");
+cli.telefone = "1234-5678";
+
+const cli2: Cliente = new Cliente("Edécio");
+
+const cli3: Cliente = cli;
+cli3.nome = "Bruna"
+
+console.table(cli);
+console.table(cli2);
+console.table(cli3);
